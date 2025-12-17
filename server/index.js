9ae0ab9026f8f29aa6209ea383ea3d6149b103c6
anshb0712 <infoforansul@gmail.com>
2025-12-17 17:35:16 +0530

    use cors and  express json middleware

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ require("dotenv").config()
 const express = require("express")
 const connectDB = require("./db/connect")
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.listen("/", (req, res) => {
+  res.send("welcome to buzz app")
+})
 app.listen(process.env.PORT || 3000, async () => {
   await connectDB(process.env.MONGO_URI)
   console.log(
