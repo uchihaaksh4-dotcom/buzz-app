9ff4438fa90afb0b50f2d8d5d9fdc515e9bf02a0
anshb0712 <infoforansul@gmail.com>
2025-12-17 18:03:02 +0530

    created an index file in error and middleware directory and install http-status-codes

---
+++ server/error/index.js
@@ -0,0 +1,35 @@
+const { StatusCodes } = require("http-status-codes")
+
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message)
+  }
+}
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message)
+    this.statusCode = StatusCodes.BAD_REQUEST
+  }
+}
+
+class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message)
+    this.statusCode = StatusCodes.NOT_FOUND
+  }
+}
+
+class UnauthenticatedError extends CustomAPIError {
+  constructor(message) {
+    super(message)
+    this.statusCode = StatusCodes.UNAUTHORIZED
+  }
+}
+
+module.exports = {
+  CustomAPIError,
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+}
