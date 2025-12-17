dd0de57a509ac434c2b46d61653bcd867853b5f5
anshb0712 <infoforansul@gmail.com>
2025-12-17 17:32:06 +0530

    create .gitignore set client side up and install cors package in the server directory

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const connectDB = require("./db/connect")
 const app = express()
 app.listen(process.env.PORT || 3000, async () => {
-  await connectDB()
+  await connectDB(process.env.MONGO_URI)
   console.log(
     `server listening on port ${process.env.PORT ? process.env.PORT : 3000}`
   )
